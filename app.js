b367f943027149c6c5b047a56290e1b44dff2cad
dprzygocka <przygocka.dagmara@gmail.com>
2021-11-12 17:43:32 +0100

    (FPG-10) - Added rooms for multiplayer

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //setup express Marianna
 const express = require("express");
+const { ClientRequest } = require("http");
+const { SocketAddress } = require("net");
 const app = express();
 
 //setup static dir
@@ -16,13 +18,74 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
+//get method for random id generation
+const { makeId } = require('./public/utils');
+
+//variable that maps client id to the room name
+const clientRoomTable = {};
+
+//global state that holds states of all posible rooms
+const state = {};
+
 io.on("connection", (socket) => {
-    setInterval(() => {
-        io.emit('new frame');
-    }, 1000 / FRAME_RATE);
+    socket.on("newGame", handleNewGameCreation);
+    socket.on("joinGame", handleJoinGame);
+
+    function handleNewGameCreation() {
+        //what we want to do is: create socketIO room and client that joins the game have to add the code which is roomId 
+        let roomName = makeId(8) //function which creates id, we pass length of the id
+
+        //new game starts so the user is assigned to the room
+        clientRoomTable[socket.id] = roomName;
+        console.log(clientRoomTable);
+        socket.emit("roomName", roomName);
+
+        //create state of the game for the room
+        state[roomName] = createNewGameState(); //we don't have this function!!!!!
+        socket.join(roomName);
+        socket.number = 1;
+        socket.emit("init", 1) // does nothing for now, we can use it to send player number to client 
+    }
+
+    function handleJoinGame(gameCode) {
+        const desiredRoom = io.sockets.adapter.rooms.get(gameCode)//we grab the room by passign its name=id
+
+        let playerNumber;
+        if (desiredRoom) {
+            playerNumber = desiredRoom.size;
+        }
+
+        if (playerNumber === 0) {
+            socket.emit("EmptyRoom");
+            return;
+        }else if (playerNumber > 3) {
+            socket.emit("FullRoom");
+            return;
+        }
+
+        clientRoomTable[socket.id] = gameCode;
+        socket.emit("roomName", gameCode);
+        socket.join(gameCode);
+        //socket.number = 
+        //socket.emit("init", 1) // does nothing for now, we can use it to send player number to client 
+        startGameInterval(gameCode);
+    
+    }
+
+    function startGameInterval (gameCode) {
+        setInterval(() => {
+            io.emit('new frame');
+        }, 1000 / FRAME_RATE);
+
+        //this method should access the roomTable access the right room by gameCode and check if someone won the game 
+    }
 
 })
-
+//just for testing if multiplayer works!!!!
+function createNewGameState() {
+    console.log("new game state created");
+    return {data: "new state"};
+}
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, (error) => {
     if (error) {
--- public/client.js
+++ public/client.js
@@ -1,20 +1,65 @@
 //setup socket
 const socket = io();
+socket.on("roomName", handleGameCodeDisplay);
+socket.on("EmptyRoom", handleEmptyRoom);
+socket.on("FullRoom", handleFullRoom);
+socket.on("init", init);
 
 //setup canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const testPlayer = new Player(0, 0, 32, 32, new Img("./assets/images/test.png", 0, 0, 0, 2, 5, 1), "username")
+//menu
+const menuScreen = document.getElementById("menuScreen");
+const gameScreen = document.getElementById("gameScreen");
 
+const createGameButton = document.getElementById("createNewGameButton");
+const codeInput = document.getElementById("codeInput");
+const joinGameButton = document.getElementById("joinGameButton");
+const dispalyGameCode = document.getElementById("dispalyGameCode");
 
+createGameButton.addEventListener("click", createGame);
+joinGameButton.addEventListener("click", joinGame);
 
-socket.on("new frame", () => {
-    draw();
-})
-
-
-function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    testPlayer.draw(ctx);
+function createGame() {
+    socket.emit("newGame");
 }
+
+function joinGame() {
+    const code = codeInput.value;
+    socket.emit("joinGame", code);
+    init();
+}
+
+//this function should be probably changed BUT REMEMBER TO LEAVE DISPLAY CHANGE!!!
+function init() {
+    menuScreen.style.display = "none";
+    gameScreen.style.display = "block";
+    const testPlayer = new Player(0, 0, 32, 32, new Img("./assets/images/test.png", 0, 0, 0, 2, 5, 1), "username")
+
+    socket.on("new frame", () => {
+        draw();
+    })
+
+    function draw(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        testPlayer.draw(ctx);
+    }
+}
+
+function handleGameCodeDisplay(gameCode) {
+    dispalyGameCode.innerText = gameCode;
+}
+
+function handleEmptyRoom() {
+    alert("The room doesn't exists!");
+    menuScreen.style.display = "block";
+    gameScreen.style.display = "none";
+}
+
+function handleFullRoom() {
+    alert("This room is full");
+    menuScreen.style.display = "block";
+    gameScreen.style.display = "none";
+}
+
