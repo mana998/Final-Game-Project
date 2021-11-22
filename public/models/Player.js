3b135637bcceedd73b54ea7f8f9554417b5202ab
Marianna <mana998@azet.sk>
2021-11-22 15:24:23 +0100

    Added gem interaction with player

--- public/models/Player.js
+++ public/models/Player.js
@@ -134,4 +134,16 @@ class Player extends GameObject { // Marianna
     const blockValue = block.split('.');
     this.score += map.coins[parseInt(blockValue[1])].value;
   }
+
+  handleGemCollision(block, row, column, map) {
+    map.tiles[row][column] = 0;
+    const blockValue = block.split('.');
+    let gem = map.gems[parseInt(blockValue[1])];
+    // based on additional value that will signal gem type
+    // can be passed in the map as 5.index.type - where types will be mapped
+    // for now just general gem
+    gem = new Gem(0, 0, 0, 0, gem.value, gem.affectsMe);
+    // call the action
+    gem.onCollect();
+  }
 }
