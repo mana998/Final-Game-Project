b76e5c3ae387275afc348536efeed615c35568a4
Marianna <mana998@azet.sk>
2021-12-07 21:03:47 +0100

    Changed connection to pool

--- private/user.js
+++ private/user.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const db = require('../database/connection').connection;
+const pool = require('../database/connection').pool;
 const bcrypt = require('bcrypt');
 
 // import utils class
@@ -15,70 +15,76 @@ const saltRounds = 15;
 // Dagmara
 // check if username and password exists in db and return player id if not return message
 router.post('/api/user/login', (req, res) => {
-  db.query('SELECT * FROM player WHERE username=?;', [req.body.username], (error, result, fields) => {
-    if (result && result.length === 1) {
-      bcrypt.compare(req.body.password, result[0].password, (error, match) => {
-        if (match) {
-          res.send({
-            playerId: result[0].player_id,
-            username: result[0].username,
-          });
-        } else {
-          res.send({
-            message: 'You have entered an invalid username or password.',
-          });
-        }
-      });
-    } else {
-      res.send({
-        message: 'You have entered an invalid username or password',
-      });
-    }
+  pool.getConnection(function(err, db) {
+    db.query('SELECT * FROM player WHERE username=?;', [req.body.username], (error, result, fields) => {
+      if (result && result.length === 1) {
+        bcrypt.compare(req.body.password, result[0].password, (error, match) => {
+          if (match) {
+            res.send({
+              playerId: result[0].player_id,
+              username: result[0].username,
+            });
+          } else {
+            res.send({
+              message: 'You have entered an invalid username or password.',
+            });
+          }
+        });
+      } else {
+        res.send({
+          message: 'You have entered an invalid username or password',
+        });
+      }
+    });
+    db.release();
   });
 });
 
 // Dagmara
 // check if username and password are valid, check if username already exsts, add new player to db
 router.post('/api/user/register', (req, res) => {
-  const usernameValid = Utils.checkStringCharacters(req.body.username);
-  const passwordValid = Utils.checkStringCharacters(req.body.password);
-  if (usernameValid && passwordValid) {
-    db.query('SELECT * FROM player WHERE username=?;', [req.body.username], (error, result, fields) => {
-      if (result && result.length === 1) {
-        res.send({
-          message: 'Username already exists. Try again.',
-        });
-      } else if (result.length === 0) {
-        bcrypt.hash(req.body.password, saltRounds, (error, hashedPassword) => {
-          if (!error) {
-            db.query('INSERT INTO player (username, password) VALUES (?, ?);', [req.body.username, hashedPassword], (error, result, fields) => {
-              if (result.affectedRows === 1) {
-                res.send({
-                  message: 'User added successfully.',
-                });
-              } else {
-                res.send({
-                  message: 'Something went wrong. Try again.',
-                });
-              }
-            });
-          } else {
-            res.send({
-              message: 'Something went wrong. Try again.',
-            });
-          }
-        });
-      } else {
-        res.send({
-          message: 'Something went wrong. Try again.',
-        });
-      }
-    });
-  } else {
-    res.send({
-      message: 'Please, use one or more characters from: A-Z and 0-9.',
-    });
-  }
+  pool.getConnection(function(err, db) {
+    const usernameValid = Utils.checkStringCharacters(req.body.username);
+    const passwordValid = Utils.checkStringCharacters(req.body.password);
+    if (usernameValid && passwordValid) {
+      db.query('SELECT * FROM player WHERE username=?;', [req.body.username], (error, result, fields) => {
+        if (result && result.length === 1) {
+          res.send({
+            message: 'Username already exists. Try again.',
+          });
+        } else if (result.length === 0) {
+          bcrypt.hash(req.body.password, saltRounds, (error, hashedPassword) => {
+            if (!error) {
+              db.query('INSERT INTO player (username, password) VALUES (?, ?);', [req.body.username, hashedPassword], (error, result, fields) => {
+                if (result.affectedRows === 1) {
+                  res.send({
+                    message: 'User added successfully.',
+                  });
+                } else {
+                  res.send({
+                    message: 'Something went wrong. Try again.',
+                  });
+                }
+              });
+            } else {
+              res.send({
+                message: 'Something went wrong. Try again.',
+              });
+            }
+          });
+        } else {
+          res.send({
+            message: 'Something went wrong. Try again.',
+          });
+        }
+      });
+    } else {
+      res.send({
+        message: 'Please, use one or more characters from: A-Z and 0-9.',
+      });
+    }
+    db.release();
+  });
 });
 
 module.exports = {
