81e76c99f3db193f2552b0272de753122f48e908
Marianna <mana998@azet.sk>
2021-12-11 16:40:29 +0100

    Split getCategoryId into separate function

--- private/interaction.js
+++ private/interaction.js
@@ -27,45 +27,44 @@ router.get('/api/interactions', (req, res) => {
   });
 });
 
-router.post('/api/interactions', async (req, res) => {
-  await addInteractions(req, res);
+router.post('/api/interactions', (req, res) => {
+  addInteractions(req, res);
 });
 
 function addInteractions(req, res) {
-  pool.getConnection(function(err, db) {
+  pool.getConnection(async function(err, db) {
     let interaction_category_id;
     //get category id
-    db.query('SELECT interaction_category_id FROM interaction_category WHERE interaction_category = ?', req.body.interaction_category, async (error, result, fields) => {
-      if (result && result.length === 1) {
-        interaction_category_id = result[0].interaction_category_id;
-        //delete messages
-        let deleteReturn = await deleteInteraction(db, req.body.player_id, interaction_category_id);
-          if (deleteReturn.result && !deleteReturn.error) {
-            //insert messages
-            let query = 'INSERT INTO interaction (interaction_message, interaction_category_id, player_id) VALUES (?, ?, ?)';
-            let values = [req.body.interactions[0], interaction_category_id, req.body.player_id];
-            for (let i = 1; i < req.body.interactions.length; i++) {
-              query += ', (?, ?, ?)';
-              values.push(req.body.interactions[i], interaction_category_id, req.body.player_id);
-            }
-            db.query(query, values, (error, result, fields) => {
-              if (result && result.affectedRows) {
-                res.send({
-                  message: "Interactions added"
-                });
-              } else {
-                res.send({
-                  message: 'Something went wrong. Try again.'
-                });
-              }
-            });
-          } else {
-            res.send({
-              message: 'Something went wrong. Try again.'
-            });
+    let result = await getCategoryId(db, req.body.interaction_category)
+    if (result && result.length === 1) {
+      interaction_category_id = result[0].interaction_category_id;
+      //delete messages
+      let deleteReturn = await deleteInteraction(db, req.body.player_id, interaction_category_id);
+        if (deleteReturn.result && !deleteReturn.error) {
+          //insert messages
+          let query = 'INSERT INTO interaction (interaction_message, interaction_category_id, player_id) VALUES (?, ?, ?)';
+          let values = [req.body.interactions[0], interaction_category_id, req.body.player_id];
+          for (let i = 1; i < req.body.interactions.length; i++) {
+            query += ', (?, ?, ?)';
+            values.push(req.body.interactions[i], interaction_category_id, req.body.player_id);
           }
-      }
-    });
+          db.query(query, values, (error, result, fields) => {
+            if (result && result.affectedRows) {
+              res.send({
+                message: "Interactions added"
+              });
+            } else {
+              res.send({
+                message: 'Something went wrong. Try again.'
+              });
+            }
+          });
+        } else {
+          res.send({
+            message: 'Something went wrong. Try again.'
+          });
+        }
+    }
     db.release();
   });
 }
@@ -90,6 +89,17 @@ async function deleteInteraction (db, player_id, category_id) {
       }
     }));
     return result;
+};
+
+async function getCategoryId(db, category) {
+  let result = await new Promise((resolve, reject) => db.query('SELECT interaction_category_id FROM interaction_category WHERE interaction_category = ?', category, (error, result, fields) => {
+    if (error) {
+      reject(error)
+    } else {
+      resolve(result)
+    }
+  }))
+  return result;
 };
 
 module.exports = {
