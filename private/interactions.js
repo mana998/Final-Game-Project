98a33f0fd9108c6612828ae1881b97c336288833
Marianna <mana998@azet.sk>
2021-12-15 00:20:19 +0100

    Added error handling

--- private/interactions.js
+++ private/interactions.js
@@ -4,26 +4,29 @@ const pool = require('../database/connection').pool;
 // Marianna
 router.get('/api/interactions', (req, res) => {
   pool.getConnection(function(err, db) {
-    if (err) console.log("error", err);
-    let interactions = {};
-    //get messages
-    let query = 'SELECT interaction_message, interaction_category FROM interaction  JOIN interaction_category ON interaction.interaction_category_id = interaction_category.interaction_category_id WHERE player_id ';
-    query += req.query.player_id ? '= ?' : 'IS NULL'
-    db.query(query, [req.query.player_id], (error, result, fields) => {
-      if (result && result.length) {
-        for (const interaction of result) {
-          if (!interactions[interaction.interaction_category]) {
-            interactions[interaction.interaction_category] = [];
+    if (err) { 
+      console.log("error", err);
+    } else {
+      let interactions = {};
+      //get messages
+      let query = 'SELECT interaction_message, interaction_category FROM interaction  JOIN interaction_category ON interaction.interaction_category_id = interaction_category.interaction_category_id WHERE player_id ';
+      query += req.query.player_id ? '= ?' : 'IS NULL'
+      db.query(query, [req.query.player_id], (error, result, fields) => {
+        if (result && result.length) {
+          for (const interaction of result) {
+            if (!interactions[interaction.interaction_category]) {
+              interactions[interaction.interaction_category] = [];
+            }
+            interactions[interaction.interaction_category].push(interaction.interaction_message);
           }
-          interactions[interaction.interaction_category].push(interaction.interaction_message);
+          res.send(interactions);
+        } else {
+          res.send({
+            message: 'No results'
+          });
         }
-        res.send(interactions);
-      } else {
-        res.send({
-          message: 'No results'
-        });
-      }
-    });
+      });
+    }
     db.release();
   });
 });
