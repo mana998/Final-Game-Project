5aa9638e0f1dae35293b77b426b908a415f5469a
Marianna <mana998@azet.sk>
2021-12-15 12:44:57 +0100

    Added cleardb database url with connection limit

--- database/connection.js
+++ database/connection.js
@@ -2,12 +2,13 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const pool  = mysql.createPool({
+const pool  = mysql.createPool(
+  process.env.CLEARDB_DATABASE_URL, {
   connectionLimit : 5,
-  host: process.env.HOST,
+  /*host: process.env.HOST,
   database: process.env.DATABASE,
   user: process.env.USER,
-  password: process.env.PASSWORD,
+  password: process.env.PASSWORD,*/
 });
 
 module.exports = {
