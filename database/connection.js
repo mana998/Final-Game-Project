9e7c6e54231c181b81d8f67a835522ec02a4b6b9
Marianna <mana998@azet.sk>
2021-12-15 11:50:32 +0100

    Removed database env

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const pool  = mysql.createPool({
   connectionLimit : 10,
   host: process.env.HOST,
-  database: process.env.DATABASE,
+  //database: process.env.DATABASE,
   user: process.env.USER,
   password: process.env.PASSWORD,
 });
