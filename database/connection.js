4b29391f8ebeb1893a77891944e05796bfd812c2
Marianna <mana998@azet.sk>
2021-12-15 12:01:22 +0100

    Added general cleardb host env

--- database/connection.js
+++ database/connection.js
@@ -4,10 +4,11 @@ const mysql = require('mysql2');
 
 const pool  = mysql.createPool({
   connectionLimit : 10,
-  host: process.env.HOST,
+  /*host: process.env.HOST,
   database: process.env.DATABASE,
   user: process.env.USER,
-  password: process.env.PASSWORD,
+  password: process.env.PASSWORD,*/
+  host: process.env.CLEARDB_DATABASE_URL
 });
 
 module.exports = {
