9510e262bffc6fa2729146102b07694a1ad3b7b2
Marianna <mana998@azet.sk>
2021-12-15 11:58:21 +0100

    Changed mysql module to mysql2

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,11 @@
 // Dagmara
 require('dotenv').config();
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const pool  = mysql.createPool({
   connectionLimit : 10,
   host: process.env.HOST,
-  //database: process.env.DATABASE,
+  database: process.env.DATABASE,
   user: process.env.USER,
   password: process.env.PASSWORD,
 });
