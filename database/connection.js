37ce3abab7fdcb582635fa97bcf41b6b08272377
Marianna <mana998@azet.sk>
2021-12-15 12:12:07 +0100

    removed cleardb env

--- database/connection.js
+++ database/connection.js
@@ -4,11 +4,10 @@ const mysql = require('mysql2');
 
 const pool  = mysql.createPool({
   connectionLimit : 10,
-  /*host: process.env.HOST,
+  host: process.env.HOST,
   database: process.env.DATABASE,
   user: process.env.USER,
-  password: process.env.PASSWORD,*/
-  host: process.env.CLEARDB_DATABASE_URL
+  password: process.env.PASSWORD,
 });
 
 module.exports = {
