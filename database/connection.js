3aa0656150f9623940980ba86eff471bbf93a14e
Marianna <mana998@azet.sk>
2021-12-15 11:39:36 +0100

    Added db env variables back

--- database/connection.js
+++ database/connection.js
@@ -4,10 +4,10 @@ const mysql = require('mysql');
 
 const pool  = mysql.createPool({
   connectionLimit : 10,
-  host: process.env.HOST2,
-  /*database: process.env.DATABASE,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
   user: process.env.USER,
-  password: process.env.PASSWORD,*/
+  password: process.env.PASSWORD,
 });
 
 module.exports = {
