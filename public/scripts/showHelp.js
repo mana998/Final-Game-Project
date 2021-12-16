24be96b55faa95dd22efbeed74f8718a64750a4f
Marianna <mana998@azet.sk>
2021-12-16 16:51:37 +0100

    Added initial help window

--- public/scripts/showHelp.js
+++ public/scripts/showHelp.js
@@ -1,3 +1,14 @@
 function showHelpWindow() {
+  $('#helpWindow').css('display', 'block');
+  if ($('#helpWindow').is(':empty')) {
+    showInitialWindow();
+  }
+}
 
+function showInitialWindow() {
+  $('#helpWindow').append(`<h1 id='helpHeader'>Placeholder</h1>`);
+  $('#helpWindow').append(`<button id='closeHelp'>X</button>`);
+  $('#helpWindow').append(`<div id='helpContent'>content</div>`);
+  $('#helpWindow').append(`<button id='rightArrow'>-&gt;</button>`);
+  $('#helpWindow').append(`<button id='leftArrow'>&lt;-</button>`);
 }
