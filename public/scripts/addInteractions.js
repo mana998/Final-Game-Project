6ef42f0a0dc44cdfe79878c2f7c2c1691b49164b
Marianna <mana998@azet.sk>
2021-12-17 00:43:50 +0100

    Added initial interactions css

--- public/scripts/addInteractions.js
+++ public/scripts/addInteractions.js
@@ -32,14 +32,13 @@ async function resetInteractions(id, category) {
 }
 
 function addInteractionField(category, interaction) {
-  return `<input type="text" class="form-control input-${category}" value='${interaction}'>`;
+  return `<input type="text" class="longInput form-control input-${category}" value='${interaction}'>`;
 }
 
 async function createInteractionsForm(id) {
+  $('#interactionForm').append(`<h1 class="gameTitle">Interactions</h1>`);
+  $('#interactionForm').append(`<div id="interactionForms">`);
   //load player interactions
-  //need to get category id and interaction id
-  //split into smaller forms based on sections
-  //need to check more how data is passed though form
   let url = `api/interactions?player_id=${id}`;
   let response = await fetch(url);
   let result = await response.json();
@@ -53,19 +52,23 @@ async function createInteractionsForm(id) {
     }
   }
   Object.keys(result).sort().map(category => {
-    $('#interactionForm').append(`<form id="${category}Form">`);
-    $(`#${category}Form`).append(`<h2>${category}</h2>`);
+    $('#interactionForms').append(`<form id="${category}Form" class="interactionForm">`);
+    $(`#${category}Form`).append(`<h2 class="categoryTitle">${category}</h2>`);
     result[category].map(interaction => {
       $(`#${category}Form`).append(addInteractionField(category, interaction));
     })
-    $(`#${category}Form`).append(`<button type="button" id="submit${category}">Save</button>`);
+    let buttons = `<div class="categoryButtons">`;
+    buttons += `<button type="button" id="submit${category}" class="submitButton">Save</button>`;
+    buttons += `<button type="button" id="reset${category}" class="resetButton">Reset</button>`;
+    buttons += `</div>`;
+    $(`#${category}Form`).append(buttons);
     $(`#submit${category}`).attr('onclick', `changeInteractions("${id}", "${category}")`);
-    $(`#${category}Form`).append(`<button type="button" id="reset${category}">Reset</button>`);
     $(`#reset${category}`).attr('onclick', `resetInteractions("${id}", "${category}")`);
-    $('#interactionForm').append(`</form>`);
+    $('#interactionForms').append(`</form>`);
   });
-  $(`#interactionForm`).append(`<button type="button" id="resetAll">Reset All</button>`);
+  $(`#interactionForm`).append(`<br><button type="button" id="resetAll" class="resetAllButton">Reset All</button>`);
   $(`#resetAll`).attr('onclick', `resetInteractions("${id}")`);
+  $(`#interactionForm`).append(`</div>`);
 }
 
 async function showInteractions(playerId) {
