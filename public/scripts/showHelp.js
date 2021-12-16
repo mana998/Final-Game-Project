b1a7aeb68ca60d7088213ed2b09795d5a2e9b48d
Marianna <mana998@azet.sk>
2021-12-16 17:56:55 +0100

    Added basic info help text

--- public/scripts/showHelp.js
+++ public/scripts/showHelp.js
@@ -1,6 +1,8 @@
 function showHelpWindow() {
   $('#helpWindow').css('display', 'block');
-  $('div').not('#helpWindow').css('filter', 'blur(0.1em)');
+  $('.container-fluid').css('filter', 'blur(0.5em)');
+  $('.container-fluid').css('transform', 'scale(1.1)');
+  //$('div').not('#helpWindow').css('filter', 'blur(0.2em)');
   if ($('#helpWindow').is(':empty')) {
     showInitialWindow();
   }
@@ -21,35 +23,54 @@ function closeHelpWindow() {
   $('#helpContent').empty();
   //close window
   $('#helpWindow').css('display', 'none');
-  $('div').not('#helpWindow').css('filter', 'blur(0em)');
+  $('.container-fluid').css('filter', 'blur(0em)');
+  $('.container-fluid').css('transform', 'scale(1)');
 }
 
 function showBasicInfo() {
   console.log('showBasicInfo');
+  $('#helpHeader').text("Basic Info");
+  $('#helpContent').empty();
   $('#leftArrow').attr('onclick', "showTraps()");
   $('#rightArrow').attr('onclick', "showControls()");
+  $('#helpContent').append(`<h2 class='helpContentHeader'>Goal</h2>`);
+  $('#helpContent').append(`<img id="chestImage" src="../assets/images/helpScreen/chest.png">`);
+  $('#helpContent').append(`<p class='helpContentText'>goal of this game is to collect as many coins as possible
+  and get to the goal as fast as possible. You will get
+  additional score based on your time. Goal is represented
+  as a chest of gold.</p>`);
+  $('#helpContent').append(`<h2 class='helpContentHeader'>Health</h2>`);
+  $('#helpContent').append(`<img id="healthImage" src="../assets/images/helpScreen/healthHelp.png">`);
+  $('#helpContent').append(`<p class='helpContentText'>you need to be careful. There are multiple traps placed
+  across the maze that will hurt you. You can see your
+  remaining health in the top left corner.
+  If your health reaches 0, you cannot continue the game</p>`);
 };
 
 function showControls() {
   console.log('showControls');
+  $('#helpContent').empty();
   $('#leftArrow').attr('onclick', "showBasicInfo()");
   $('#rightArrow').attr('onclick', "showCollectibles()");
 };
 
 function showCollectibles() {
   console.log('showCollectibles');
+  $('#helpContent').empty();
   $('#leftArrow').attr('onclick', "showControls()");
   $('#rightArrow').attr('onclick', "showGemTypes()");
 };
 
 function showGemTypes() {
   console.log('showGemTypes');
+  $('#helpContent').empty();
   $('#leftArrow').attr('onclick', "showCollectibles()");
   $('#rightArrow').attr('onclick', "showTraps()");
 };
 
 function showTraps() {
   console.log('showTraps');
+  $('#helpContent').empty();
   $('#leftArrow').attr('onclick', "showGemTypes()");
   $('#rightArrow').attr('onclick', "showBasicInfo()");
 };
