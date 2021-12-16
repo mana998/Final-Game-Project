19d92af76b3ea0edc45fd51b21ad77cb14e1bc85
Marianna <mana998@azet.sk>
2021-12-16 17:03:06 +0100

    Added close help functionality

--- public/scripts/showHelp.js
+++ public/scripts/showHelp.js
@@ -4,6 +4,7 @@ function showHelpWindow() {
   if ($('#helpWindow').is(':empty')) {
     showInitialWindow();
   }
+  showBasicInfo();
 }
 
 function showInitialWindow() {
@@ -12,4 +13,33 @@ function showInitialWindow() {
   $('#helpWindow').append(`<div id='helpContent'>content</div>`);
   $('#helpWindow').append(`<button id='rightArrow'>-&gt;</button>`);
   $('#helpWindow').append(`<button id='leftArrow'>&lt;-</button>`);
+  $('#closeHelp').on('click', closeHelpWindow);
 }
+
+function closeHelpWindow() {
+  //empty content
+  $('#helpContent').empty();
+  //close window
+  $('#helpWindow').css('display', 'none');
+  $('div').not('#helpWindow').css('filter', 'blur(0em)');
+}
+
+function showBasicInfo() {
+  console.log('showBasicInfo');
+};
+
+function showControls() {
+  console.log('showControls');
+};
+
+function showCollectibles() {
+  console.log('showCollectibles');
+};
+
+function showGemTypes() {
+  console.log('showGemTypes');
+};
+
+function showTraps() {
+  console.log('showTraps');
+};
