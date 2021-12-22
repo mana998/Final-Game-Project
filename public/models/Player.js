13efeb478288dabffd975edc8a932bdac38a81c2
dprzygocka <przygocka.dagmara@gmail.com>
2021-12-22 11:14:03 +0100

    delete coins field from player

--- public/models/Player.js
+++ public/models/Player.js
@@ -4,7 +4,6 @@ class Player extends GameObject { // Marianna
   constructor(x, y, width, height, img, username, message, socketId) {
     super(x, y, width, height, img);
     this.username = username;
-    this.coins = 0;
     this.health = 10;
     this.score = 0;
     this.speed = 4;
