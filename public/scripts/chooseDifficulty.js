457506f0d6a2911a80fca6fcef9259800674e60c
Marianna <mana998@azet.sk>
2021-12-26 18:45:58 +0100

    Cleaned up chooseDifficulty.js with eslint

--- public/scripts/chooseDifficulty.js
+++ public/scripts/chooseDifficulty.js
@@ -1,20 +1,24 @@
-function openDifficultyScreen () {
-    $('#mainMenu').css('display', 'none');
-    $('#returnToMainMenuButton').css('display', 'block');
-    $('#difficultySelection').css('display', 'block'); 
-    createDifficultyScreen();  
-    $('#loggedInUserIcon').css('display', 'none');
+// Marianna
+function createDifficultyScreen() {
+  if (!$('#difficultySelection').children().length) {
+    $('#difficultySelection').append(`
+            <h1 class="gameTitle">CHOOSE YOUR DIFFICULTY:</h1>
+            <button type="button" class="difficultyButton backgroundPicture smallButton" onClick="createGame(0)">
+              <span class="buttonText orangeText">EASY</span>
+            </button>
+            <button type="button" class="difficultyButton backgroundPicture smallButton" onClick="createGame(1)">
+              <span class="buttonText orangeText">HARD</span>
+            </button>
+        `);
+  }
 }
 
-function createDifficultyScreen() {
-    if (!$('#difficultySelection').children().length) {
-        $('#difficultySelection').append(`
-            <h1 class="gameTitle">CHOOSE YOUR DIFFICULTY:</h1>
-            <button type="button" class="difficultyButton backgroundPicture smallButton" onClick="createGame(0)"><span class="buttonText orangeText">EASY</span></button>
-            <button type="button" class="difficultyButton backgroundPicture smallButton" onClick="createGame(1)"><span class="buttonText orangeText">HARD</span></button>
-        `);
-    } 
+function openDifficultyScreen() {
+  $('#mainMenu').css('display', 'none');
+  $('#returnToMainMenuButton').css('display', 'block');
+  $('#difficultySelection').css('display', 'block');
+  createDifficultyScreen();
+  $('#loggedInUserIcon').css('display', 'none');
 }
 
 $('#createNewGameButton').on('click', openDifficultyScreen);
-
